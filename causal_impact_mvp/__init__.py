3a69813fd6cc6f20f25a3927fc1c6e2a5388e9cb
Peijun Xu <36050073+bard259@users.noreply.github.com>
2025-08-28 23:50:15 -0700

    Update __init__.py

--- causal_impact_mvp/__init__.py
+++ causal_impact_mvp/__init__.py
@@ -1,17 +1,2 @@
-from .core import (
-    dataloader,
-    syn_generate,
-    assumption_val,
-    causal_impact,
-    casual_impact,
-    run_demo,
-)
-
-__all__ = [
-    "dataloader",
-    "syn_generate",
-    "assumption_val",
-    "causal_impact",
-    "casual_impact",
-    "run_demo",
-]
+def run_demo():
+    return "Demo ran!"
